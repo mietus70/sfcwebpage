da4dd9027c85acddfd0c93179a0460efeb89f216
mietus70 <mietus70@gmail.com>
2018-03-25 22:26:00 +0200

    Wygenerowanie głównych komponentów

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ElementComponent } from './components/element/element/element.component';
+import { FooterComponent } from './components/elements/main-elements/footer/footer.component';
+import { HeaderComponent } from './components/elements/main-elements/header/header.component';
+import { TopMenuComponent } from './components/elements/main-elements/top-menu/top-menu.component';
+import { PageComponent } from './components/page/page.component';
+import { ErrorPageComponent } from './components/pages/error-page/error-page.component';
+import { MainPageComponent } from './components/pages/main-page/main-page.component';
+import { WidgetComponent } from './components/widget/widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElementComponent,
+    FooterComponent,
+    HeaderComponent,
+    TopMenuComponent,
+    PageComponent,
+    ErrorPageComponent,
+    MainPageComponent,
+    WidgetComponent
   ],
   imports: [
     BrowserModule
