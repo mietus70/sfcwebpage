1aab6a399a545e05722db8540eb9bb51684b9b91
mietus70 <mietus70@gmail.com>
2018-04-23 14:52:05 +0200

    Dodanie zmiennych do typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,16 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
 declare var modernizr: any;
+declare var module: NodeModule;
+declare var timer: any;
+declare var ease: any;
+declare var velocity: any;
+declare var main: any;
+declare var form: any;
+declare var jq: any;
+declare var jqueryui: any;
+declare var theme: any;
+declare var type: any;
 
 interface NodeModule {
   id: string;
