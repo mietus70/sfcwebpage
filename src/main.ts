fdec6efb7d1a8b275a75cf23358137cb02a2e2be
Pawel Grzyb <mietus70@gmail.com>
2020-03-31 15:36:34 +0200

    Add DebugTools and remove google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
-import { AppBrowserModule } from '.././src/app/app.browser.module';
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppBrowserModule }               from './app/app.browser.module';
+import { ApplicationRef, enableProdMode } from '@angular/core';
+import { platformBrowserDynamic }         from '@angular/platform-browser-dynamic';
+//import { AppModule } from './app/app.module';
+import { environment }                    from './environments/environment';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { enableDebugTools } from "@angular/platform-browser";
 
-enableProdMode();
+// enableProdMode();
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppBrowserModule);
+platformBrowserDynamic().bootstrapModule(AppBrowserModule).then((
+  module => {
+    let applicationRef = module.injector.get(ApplicationRef);
+    let appComponent = applicationRef.components[ 0 ];
+    enableDebugTools(appComponent);
+  }
+));
