d8c09df2ac36cc250e015d74671fa60422799f39
Pawel Grzyb <mietus70@users.noreply.github.com>
2026-09-04 20:20:42 +0200

    fix: enable hash-based routing, add missing deps, fix SSR server path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -28,8 +28,7 @@ export const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    // RouterModule.forRoot(appRoutes, {useHash: true})
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, {useHash: true})
   ],
   exports: [RouterModule]
 })
